f2d2046c6a55870bf15c8cfaf1166788466577a5
Cobaile8 <connor.baileyyy@gmail.com>
2019-03-04 00:39:20 -0500

    More comments

--- index.php
+++ index.php
@@ -9,45 +9,53 @@ if(!isset($_SESSION["type"]))
 }
 
 include('header.php');
-
 ?>
+
+
+
 	<br />
 	<div class="row">
 	<?php
 	if($_SESSION['type'] == 'master')	
 	{
 	?>
-		<!-- 
-			This function should show the TOTAL number of employees with accounts (excluding master admin users)
-		-->
-	<div class="col-md-4">
-		<div class="panel panel-default">
-			<div class="panel-heading"><strong>Total Employees</strong></div>
-			<div class="panel-body" align="center">
-				<h1><?php echo count_total_user($connect); ?></h1>
+			<!-- 
+				NEEDS WORK
+
+				This function should show the TOTAL number of employees with accounts (excluding master admin users)
+			-->
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Total Employees</strong></div>
+				<div class="panel-body" align="center">
+					<h1><?php echo count_total_user($connect); ?></h1>
+				</div>
 			</div>
 		</div>
-	</div>
 
-		<!-- 
-			This function should show the TOTAL number of pieces of equipment checked out by ALL employees (only seen by admin).
+			<!-- 
+				NEEDS WORK
 
-			That is to say, when an employee checks out a piece of equipment, this number should increment, and when the check it back in, it should decrement.
-		-->
-	<div class="col-md-4">
-		<div class="panel panel-default">
-			<div class="panel-heading"><strong>Total Equipment Checked In</strong></div>
-			<div class="panel-body" align="center">
-				<h1><?php echo count_total_product($connect); ?></h1>
+				This function should show the TOTAL number of pieces of equipment checked out by ALL employees (only seen by admin).
+
+				That is to say, when an employee checks out a piece of equipment, this number should increment, and when the check it back in, it should decrement.
+			-->
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading"><strong>Total Equipment Checked In</strong></div>
+				<div class="panel-body" align="center">
+					<h1><?php echo count_total_product($connect); ?></h1>
+				</div>
 			</div>
 		</div>
-	</div>
 
 	<?php
 	}
 	?>
 
 			<!-- 
+				NEEDS WORK
+
 				This function should show the number of pieces of equipment checked out by the employee who owns the account. 
 
 				That is to say, when the employee currently logged in checks out a piece of equipment, this number should increment, and when they check it back in, it should decrement.
@@ -91,21 +99,23 @@ include('header.php');
 		{
 		?>
 
-			<!-- 
-				This function should show a table that lists the following:
-				- Employee#
-				- Employee name (first and last)
-				- SiteID
-				- Equipment_id
-			-->
-		<div class="col-md-12">
-			<div class="panel panel-default">
-				<div class="panel-heading"><strong>Total Order Value User wise</strong></div>
-				<div class="panel-body" align="center">
-					<?php echo get_user_wise_total_order($connect); ?>
+				<!-- 
+					NEEDS WORK
+
+					This function should show a table that lists the following:
+					- Employee#
+					- Employee name (first and last)
+					- SiteID
+					- Equipment_id
+				-->
+			<div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-heading"><strong>Total Order Value User wise</strong></div>
+					<div class="panel-body" align="center">
+						<?php echo get_user_wise_total_order($connect); ?>
+					</div>
 				</div>
 			</div>
-		</div>
 		<?php
 		}
 		?>

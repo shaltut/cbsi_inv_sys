56a9fa9abcd4d6a776162301289fd4e2771f89b8
Cobaile8 <connor.baileyyy@gmail.com>
2019-03-09 03:34:34 -0500

    Figured out what most of the functions in the function.php page do. Still needs some work.

--- function.php
+++ function.php
@@ -1,6 +1,13 @@
 <?php
 //function.php
 
+
+/* 
+	********** NEEDS WORK **********
+	-Figure out what this function does and where it is used.
+
+	-When you comment it out, and test the system, the equipment page (product.php) gets screwy... Might be a good place to start.
+*/
 function fill_category_list($connect)
 {
 	$query = "
@@ -19,6 +26,9 @@ function fill_category_list($connect)
 	return $output;
 }
 
+/* 
+	This function returns a user_name from the user_details table given any valid user_id from the user_details table.
+*/
 function get_user_name($connect, $user_id)
 {
 	$query = "
@@ -33,6 +43,10 @@ function get_user_name($connect, $user_id)
 	}
 }
 
+/* 
+	********** NEEDS WORK **********
+	- Figure out what this function does, and where it is used in the system.
+*/
 function fill_product_list($connect)
 {
 	$query = "
@@ -51,24 +65,32 @@ function fill_product_list($connect)
 	return $output;
 }
 
-function fetch_product_details($product_id, $connect)
-{
-	$query = "
-	SELECT * FROM product 
-	WHERE product_id = '".$product_id."'";
-	$statement = $connect->prepare($query);
-	$statement->execute();
-	$result = $statement->fetchAll();
-	foreach($result as $row)
-	{
-		$output['product_name'] = $row["product_name"];
-		$output['quantity'] = $row["product_quantity"];
-		$output['price'] = $row['product_base_price'];
-		$output['tax'] = $row['product_tax'];
-	}
-	return $output;
-}
+/* 
+	This function returns the product_name, quantity, price, and tax from the product table.
+*/
+// function fetch_product_details($product_id, $connect)
+// {
+// 	$query = "
+// 	SELECT * FROM product 
+// 	WHERE product_id = '".$product_id."'";
+// 	$statement = $connect->prepare($query);
+// 	$statement->execute();
+// 	$result = $statement->fetchAll();
+// 	foreach($result as $row)
+// 	{
+// 		$output['product_name'] = $row["product_name"];
+// 		$output['quantity'] = $row["product_quantity"];
+// 		$output['price'] = $row['product_base_price'];
+// 		$output['tax'] = $row['product_tax'];
+// 	}
+// 	return $output;
+// }
 
+/* 
+	This function is currently being used to display the "22" number on index.php
+
+	It returns the number of items in inventory (adds up the "quantity" value for each product available)
+*/
 function available_product_quantity($connect, $product_id)
 {
 	$product_data = fetch_product_details($product_id, $connect);
@@ -168,6 +190,11 @@ function count_total_product($connect)
 	return $statement->rowCount();
 }
 
+/* 
+	This function is currently being used to display the "$32,107.35" number on index.php
+
+	It returns the sum total of all money spent on inventory_order's that have an active status
+*/
 function count_total_order_value($connect)
 {
 	$query = "
@@ -187,6 +214,11 @@ function count_total_order_value($connect)
 	}
 }
 
+/* 
+	This function is currently being used to assist get_user_wise_total_order()
+
+	It returns the number of inventory_order's that were paid for using 'cash'
+*/
 function count_total_cash_order_value($connect)
 {
 	$query = "
@@ -207,6 +239,11 @@ function count_total_cash_order_value($connect)
 	}
 }
 
+/* 
+	This function is currently being used to assist get_user_wise_total_order()
+
+	It returns the number of inventory_order's that were paid for using 'credit'
+*/
 function count_total_credit_order_value($connect)
 {
 	$query = "
@@ -231,6 +268,31 @@ Creates the table on Index.php that displays the "Total Order Value User Wise"
 Calls the count_total_credit_order_value() function 
 Calls the count_total_cash_order_value() function
 
+
+********** CHANGES NEEDED **********
+- needs to print out all pieces of equipment that are currently checked out by each user.
+	--Something like:
+		SELECT e.empl_id, e.empl_firstname||' '||e.empl_lastname as "empl_name", eq.equip_id, eq.equip_type, c.check_out_date_time
+		FROM employees e INNER JOIN check_employee_equipment eq using (empl_id)
+		JOIN equipment USING (equip_id)
+		WHERE equip_status = 'checked out'
+
+	-- Changes to be made to the database for this to be possible:
+
+		- A "check_employee_equipment" table must be added to the DB:
+			This table will be a bridge between the employees table and the 
+			equipment table, and will hold information about check-outs, 
+			including -> (check_id (PK), empl_id (FK), equip_id (FK), check_date_time)
+
+		- The user_details table has to be changed to "employees"
+			- Everywhere it says "user_%" in this table should be changed to "empl_%"
+
+		- The product table has to be changed to "equipment"
+			- Everywhere it says "product_%" in this table should be changed to "equip_%"
+
+		- user_name (or "empl_name" after changed) in the user_details table (or "equipment" after changed) has to be split into "empl_firstname" and "empl_lastname"
+		- The new equipment table must be modified to include an equip_type column
+******************************************
 */
 function get_user_wise_total_order($connect)
 {

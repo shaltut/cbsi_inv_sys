3246131ac2ffa5c2bb57355066e33964cd99651d
nestorp23 <pnestor823@gmail.com>
2019-03-26 13:49:54 -0400

    Should hide errors involving code, it is in database_connection.php since every file references it

--- database_connection.php
+++ database_connection.php
@@ -1,6 +1,9 @@
 <?php
 //database_connection.php
 
+// Hides errors that display information about the code EXAMPLE(Notice: Undefined index: e in C:\xampp\htdocs\cbsi_inv_sys(test)\equipment_action.php on line 22
+ini_set('display_errors', '0');
+
 $connect = new PDO('mysql:host=localhost;dbname=cbsi_db', 'root', '');
 session_start();
 

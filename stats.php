e5507a2ee50b5afb199292a3d3ad49ea9ef47ccb
Cobaile8 <connor.baileyyy@gmail.com>
2019-03-27 18:24:19 -0400

    removed jQuery and Javascript functions from stats.php since they arent being used there

--- stats.php
+++ stats.php
@@ -23,51 +23,37 @@ include('header.php');
 
 <button onclick="goBack()" style="float:right;">Go Back</button>
 
-
-
-         <!-- Commented out stats cards in favor of stats page --> 
-
-            
-
-         <center><h2 class="decorated"><span>Site Stats</span></h2></center>
-        
-
-
-            <div class="row">
-            <div class="col-lg-12">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-lg-10 col-md-10 col-sm-8 col-xs-6">
-                                <h3 class="panel-title">Site Stats</h3>
-                            </div>
-                        
-                            <div class="col-lg-2 col-md-2 col-sm-4 col-xs-6" align='right'>
-                                
-                            </div>
-                        </div>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row"><div class="col-sm-12 table-responsive">
-                            <table id="equipment_data" class="table table-bordered table-striped">
-                                <thead><tr>
-                                    <th>Active Sites</th>
-                                    <th><?php echo count_master_active($connect); ?></th>
-                                </tr></thead>
-                                <thead><tr>
-                                    <th>Inactive Sites</th>
-                                    <th><?php echo count_equipment_total($connect); ?></th>
-                                </tr></thead>
-                            </table>
-                        </div></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+<center><h2 class="decorated"><span>Site Stats</span></h2></center>
+<div class="row">
+	<div class="col-lg-12">
+	    <div class="panel panel-default">
+	        <div class="panel-heading">
+	            <div class="row">
+	                <div class="col-lg-10 col-md-10 col-sm-8 col-xs-6">
+	                    <h3 class="panel-title">Site Stats</h3>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="panel-body">
+	            <div class="row"><div class="col-sm-12 table-responsive">
+	                <table id="equipment_data" class="table table-bordered table-striped">
+	                    <thead><tr>
+	                        <th>Active Sites</th>
+	                        <th><?php echo count_master_active($connect); ?></th>
+	                    </tr></thead>
+	                    <thead><tr>
+	                        <th>Inactive Sites</th>
+	                        <th><?php echo count_equipment_total($connect); ?></th>
+	                    </tr></thead>
+	                </table>
+	            </div></div>
+	        </div>
+	    </div>
+	</div>
+</div>
 
 
 <center><h2 class="decorated"><span>Employee Stats</span></h2></center>
-
 <div class="row">
             <div class="col-lg-12">
                 <div class="panel panel-default">
@@ -105,7 +91,6 @@ include('header.php');
         </div>
 
 <center><h2 class="decorated"><span>Equipment Stats</span></h2></center>
-
 <div class="row">
             <div class="col-lg-12">
                 <div class="panel panel-default">
@@ -144,128 +129,4 @@ include('header.php');
 
 
 
-            
-
-
-
-<script>
-$(document).ready(function(){
-    var sitedataTable = $('#site_data').DataTable({
-        "processing":true,
-        "serverSide":true,
-        "order":[],
-        "ajax":{
-            url:"site_fetch.php",
-            type:"POST"
-        },
-        "columnDefs":[
-            {
-
-                "targets":[4, 5, 6],
-                "orderable":false,
-            },
-        ],
-        "pageLength": 6
-    });
-
-    $('#add_button').click(function(){
-        $('#siteModal').modal('show');
-        $('#site_form')[0].reset();
-        $('.modal-title').html("<i class='fa fa-plus'></i> Add Item");
-        $('#action').val("Add");
-        $('#btn_action').val("Add");
-    });
-
-    $(document).on('submit', '#site_form', function(event){
-        event.preventDefault();
-        $('#action').attr('disabled', 'disabled');
-        var form_data = $(this).serialize();
-        $.ajax({
-            url:"site_action.php",
-            method:"POST",
-            data:form_data,
-            success:function(data)
-            {
-                $('#site_form')[0].reset();
-                $('#siteModal').modal('hide');
-                $('#alert_action').fadeIn().html('<div class="alert alert-success">'+data+'</div>');
-                $('#action').attr('disabled', false);
-                sitedataTable.ajax.reload();
-            }
-        })
-    });
-
-    $(document).on('click', '.view', function(){
-        var site_id = $(this).attr("id");
-        var btn_action = 'site_details';
-        $.ajax({
-            url:"site_action.php",
-            method:"POST",
-            data:{site_id:site_id, btn_action:btn_action},
-            success:function(data){
-                $('#sitedetailsModal').modal('show');
-                $('#site_details').html(data);
-            }
-        })
-    });
-
-    $(document).on('click', '.update', function(){
-        var site_id = $(this).attr("id");
-        var btn_action = 'fetch_single';
-        $.ajax({
-            url:"site_action.php",
-            method:"POST",
-            data:{site_id:site_id, btn_action:btn_action},
-            dataType:"json",
-            success:function(data){
-                $('#siteModal').modal('show');
-                $('#site_name').val(data.site_name);
-                $('#site_address').val(data.site_address);
-                $('#job_desc').val(data.job_desc);
-                $('#start_date').val(data.start_date);
-                $('.modal-title').html("<i class='fa fa-pencil-square-o'></i> Edit Site");
-                $('#site_id').val(site_id);
-                $('#action').val("Edit");
-                $('#btn_action').val("Edit");
-            }
-        })
-    });
-
-    $(document).on('click', '.delete', function(){
-        var site_id = $(this).attr("id");
-        var status = $(this).data("status");
-        var btn_action = 'delete';
-        if(confirm("Are you sure you want to change status?"))
-        {
-            $.ajax({
-                url:"site_action.php",
-                method:"POST",
-                data:{site_id:site_id, status:status, btn_action:btn_action},
-                success:function(data){
-                    $('#alert_action').fadeIn().html('<div class="alert alert-info">'+data+'</div>');
-                    sitedataTable.ajax.reload();
-                }
-            });
-        }
-        else
-        {
-            return false;
-        }
-    });
-
-});
-</script>
-
-<script>
-
-    //Used to toggle the 'view stats' button 
-    function buttontext() {
-        if(document.getElementById("site_stat_btn").value === "Show Site Stats")
-            document.getElementById("site_stat_btn").value = "Hide Site Stats";
-        else
-            document.getElementById("site_stat_btn").value = "Show Site Stats";
-    }
-    function goBack() {
-  		window.history.back();
-	}
-</script>
+           

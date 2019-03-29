6efacb5cc885ea2f47c6a95c78292681b4b910dd
Cobaile8 <connor.baileyyy@gmail.com>
2019-03-29 19:18:29 -0400

    Made it so you cant check out a piece of equipment without selecting a site

--- chk_out_action.php
+++ chk_out_action.php
@@ -20,39 +20,45 @@ if(isset($_POST['btn_action']))
         $chk_avail = check_equip_availability($connect, $_POST['equip_id']);
         
         if($chk_equip_id == 'yes'){
-        	if($chk_avail){
+        	if($_POST['site_id'] >= 303000){
+	        	if($chk_avail){
 
-	            $query = "
-	            INSERT INTO equipment_checkout (equip_id, empl_id, site_id, chk_date_time) 
-	            VALUES (:equip_id, :empl_id, :site_id, :chk_date_time);
+		            $query = "
+		            INSERT INTO equipment_checkout (equip_id, empl_id, site_id, chk_date_time) 
+		            VALUES (:equip_id, :empl_id, :site_id, :chk_date_time);
 
-	            UPDATE equipment
-	            SET is_available = 'unavailable'
-	            WHERE equip_id = '".$_POST['equip_id']."'
-	            ";  
-	            $statement = $connect->prepare($query);
-	            $statement->execute(
-	                array(
-	                    ':equip_id'         =>  $_POST["equip_id"],
-	                    ':empl_id'          =>  $_SESSION["user_id"],
-	                    ':site_id'          =>  $_POST["site_id"],
-	                    ':chk_date_time'    =>  date("Y-m-d h:i a")
-	                    
-	                )
-	            );
+		            UPDATE equipment
+		            SET is_available = 'unavailable'
+		            WHERE equip_id = '".$_POST['equip_id']."'
+		            ";  
+		            $statement = $connect->prepare($query);
+		            $statement->execute(
+		                array(
+		                    ':equip_id'         =>  $_POST["equip_id"],
+		                    ':empl_id'          =>  $_SESSION["user_id"],
+		                    ':site_id'          =>  $_POST["site_id"],
+		                    ':chk_date_time'    =>  date("Y-m-d h:i a")
+		                    
+		                )
+		            );
 
 
-	            //Verifying that the database was updated successfully
-	            $result = $statement->fetchAll();
-	            if(isset($result))
-	            {
-	                echo 'Successfully checked out an item!';
-	            }else{
-	                echo 'Something went wrong!';
-	            }
-	        }else{
-	        	echo 'ERROR! This Item is in use by someone else!';
-	        }
+		            //Verifying that the database was updated successfully
+		            $result = $statement->fetchAll();
+		            if(isset($result))
+		            {
+		                echo 'Successfully checked out an item!';
+		            }else{
+		                echo 'Something went wrong!';
+		            }
+		        }else{
+		        	echo 'ERROR! This Item is in use by someone else!';
+		        }
+		    }elseif ($_POST['site_id'] == 0) {
+		    	echo 'ERROR - Please select a site!';
+		    }else{
+		    	echo 'ERROR - The site you selected is incorrect, or no longer in operation!';
+		    }
         }else{
             echo $chk_equip_id;
         }

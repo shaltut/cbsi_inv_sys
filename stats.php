bf4fe8bb8e3a4536e14272055115b79402a2a476
Cobaile8 <connor.baileyyy@gmail.com>
2019-04-09 23:28:12 -0400

    test me

--- stats.php
+++ stats.php
@@ -24,18 +24,18 @@ include('header.php');
         5,7,6,6,2,1,
         7,12,8,6,2,1, 
 */
-echo    nl2br(checkouts_by_site_names($connect)."\n".
-        checkouts_by_site_num_checkouts_today($connect)."\n".
-        checkouts_by_site_num_checkouts_week($connect)." \n".
-        checkouts_by_site_num_checkouts_month($connect)."\n".
-        checkouts_by_site_num_checkouts($connect));
+// echo    nl2br(checkouts_by_site_names($connect)."\n".
+//         checkouts_by_site_num_checkouts_today($connect)."\n".
+//         checkouts_by_site_num_checkouts_week($connect)." \n".
+//         checkouts_by_site_num_checkouts_month($connect)."\n".
+//         checkouts_by_site_num_checkouts($connect));
 
 
 ?>
 <!-- Alerts the user to changes they have made, or errors -->
 <span id='alert_action'></span>
 
-<button class="btn btn-info" onclick="goBack()" style="float:right;">Go Back</button>
+<!-- <button class="btn btn-info" onclick="goBack()" style="float:right;">Go Back</button> -->
 
 <br/><br/><br/>
 
@@ -55,37 +55,37 @@ echo    nl2br(checkouts_by_site_names($connect)."\n".
 	                <table id="site_stats_tbl" class="table table-bordered table-striped">
 	                    <thead><tr>
 	                        <th>Active Sites</th>
-	                       <th><?php echo count_active_site($connect); ?></th>
+	                       <!-- <th><?php //echo count_active_site($connect); ?></th> -->
 	                    </tr></thead>
 	                    <thead><tr>
 	                        <th>Inactive Sites</th>
-	                        <th><?php echo count_inactive_site($connect); ?></th>
+	                        <!-- <th><?php //echo count_inactive_site($connect); ?></th> -->
 	                    </tr></thead>
                         <?php
-                        if(total_sites_VA($connect) > 0){
+                        // if(total_sites_VA($connect) > 0){
                         ?>
-                            <thead><tr>
+                            <!-- <thead><tr>
                                 <th>Total Sites In Virginia</th>
-                                <th><?php echo total_sites_VA($connect); ?></th>
-                            </tr></thead>
+                                <th><?php //echo total_sites_VA($connect); ?></th>
+                            </tr></thead> -->
                         <?php
-                        }
-                        if(total_sites_DC($connect) > 0){
+                        // }
+                        //if(total_sites_DC($connect) > 0){
                         ?>
-                        <thead><tr>
+                        <!-- <thead><tr>
                             <th>Total Sites In Washington DC</th>
-                            <th><?php echo total_sites_DC($connect); ?></th>
-                        </tr></thead>
+                            <th><?php //echo total_sites_DC($connect); ?></th>
+                        </tr></thead> -->
                         <?php
-                        }
-                        if(total_sites_MD($connect) > 0){
+                        //}
+                        //if(total_sites_MD($connect) > 0){
                         ?>
-                        <thead><tr>
+                        <!-- <thead><tr>
                             <th>Total Sites In Maryland</th>
-                            <th><?php echo total_sites_MD($connect); ?></th>
-                        </tr></thead>
-                        <?php
-                        }
+                            <th><?php //echo total_sites_MD($connect); ?></th>
+                        </tr></thead> -->
+                       <?php
+                        //}
                         ?>
 	                </table>
 	            </div></div>
@@ -119,15 +119,15 @@ echo    nl2br(checkouts_by_site_names($connect)."\n".
                     <table id="empl_stats_tbl" class="table table-bordered table-striped">
                         <thead><tr>
                             <th>Number of Employees Registered</th>
-                            <th><?php echo count_employee_total($connect); ?></th>
+                            <!-- <th><?php //echo count_employee_total($connect); ?></th> -->
                         </tr></thead>
                         <thead><tr>
                             <th>Number of Admin Accounts</th>
-                            <th><?php echo count_master_active($connect); ?></th>
+                            <!-- <th><?php //echo count_master_active($connect); ?></th> -->
                         </tr></thead>
                         <thead><tr>
                             <th>Total Users</th>
-                            <th><?php echo count_total_user_active($connect); ?></th>
+                            <!-- <th><?php //echo count_total_user_active($connect); ?></th> -->
                         </tr></thead>
                     </table>
                     <br/>
@@ -135,9 +135,9 @@ echo    nl2br(checkouts_by_site_names($connect)."\n".
                         <label for="empl_select" class="center">Select User to View Stats:</label>
                         <center>
                             <select class="form-control center" name="empl_id" id="empl_id" style="width:85%; display:inline">
-                                <?php
-                                    echo users_options($connect);
-                                ?>
+                                <!-- <?php
+                                    //echo users_options($connect);
+                                ?> -->
                             </select>
                             <input type="hidden" name="btn_action" id="btn_action"value="user_stat"/>
                             <input type="submit" name="emp_go" id="emp_go" class="btn btn-success" value="GO" onClick="submitEmplStats()"/>
@@ -174,31 +174,33 @@ echo    nl2br(checkouts_by_site_names($connect)."\n".
                     <table id="equip_stats_tbl" class="table table-bordered table-striped">
                         <thead><tr>
                             <th>Equipment Total</th>
-                            <th><?php echo count_equipment_total($connect); ?></th>
+                            <!-- <th><?php //echo count_equipment_total($connect); ?></th> -->
                         </tr></thead>
                         <thead><tr>
                             <th>Equipment Checked Out</th>
-                            <th><?php echo count_check_out_total($connect); ?></th>
+                            <!-- <th><?php //echo count_check_out_total($connect); ?></th> -->
                         </tr></thead>
                         <thead><tr>
                             <th>Equipment Needing Maintenance</th>
                             <th><?php
-                                $mCount = maintenance_red_num($connect); 
-                                echo $mCount;
-                                if($mCount > 0){
-                                    echo "<a class=\"btn btn-xs btn-danger\" href=\"maintain.php\" role=\"button\" style=\"float:right; margin-right:10px;\">View</a>";
-                                }else{
-                                    echo "NOPE";
-                                }
+                                // $mCount = maintenance_red_num($connect); 
+                                // echo $mCount;
+                                // if($mCount > 0){
+                                //      echo "<a class=\"btn btn-xs btn-danger\" href=\"maintain.php\" role=\"button\" style=\"float:right; margin-right:10px;\">View</a>";
+                                // }else{
+                                //     //echo "NOPE";
+                                // }
                             ?>    
                             </th>
                         </tr></thead>
                         <thead><tr>
                             <th>Total Cost of All Equipment</th>
-                            <th><?php 
-                                setlocale(LC_MONETARY, 'en_US');
-                                echo money_format('%(#10n', total_equipment_cost($connect)); 
-                            ?></th>
+                            <th>
+                            <!-- <?php 
+                                //setlocale(LC_MONETARY, 'en_US');
+                                //echo money_format('%(#10n', total_equipment_cost($connect)); 
+                            ?> -->
+                            </th>
                         </tr></thead>
                     </table>
                 </div></div>
@@ -225,18 +227,18 @@ echo    nl2br(checkouts_by_site_names($connect)."\n".
     include('charts_js.php');
 ?>
 <script>
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $('#user_stats_form').on('change', function() {
-        var $form = $(this).closest('form');
-        $form.find('input[type=submit]').click();
-    });
+//     $('#user_stats_form').on('change', function() {
+//         var $form = $(this).closest('form');
+//         $form.find('input[type=submit]').click();
+//     });
 
-});
+// });
 </script>
 <script>
-function goBack() {
-  window.history.back();
-}
+// function goBack() {
+//   window.history.back();
+// }
 </script>
            

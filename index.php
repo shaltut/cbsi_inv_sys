253c3883b0daf5ffcaa60e3ee8e40e9e730701c0
Cobaile8 <connor.baileyyy@gmail.com>
2019-04-10 21:07:19 -0400

    Made the table in index.php that shows your currently checked out equipment universal (users and masters)

--- index.php
+++ index.php
@@ -23,7 +23,9 @@ include('header.php');
 		-->
 		<div class="col-md-4">
 			<div class="panel panel-default">
-				<div class="panel-heading"><strong>Total Employees</strong></div>
+				<div class="panel-heading">
+					<strong>Total Employees</strong>
+				</div>
 				<div class="panel-body" align="center">
 					<h1><?php echo count_employee_total($connect); ?></h1>
 				</div>
@@ -35,7 +37,9 @@ include('header.php');
 		-->
 		<div class="col-md-4">
 			<div class="panel panel-default">
-				<div class="panel-heading"><strong>Total Equipment Checked Out</strong></div>
+				<div class="panel-heading">
+					<strong>Total Equipment Checked Out</strong>
+				</div>
 				<div class="panel-body" align="center">
 					<h1><?php echo count_check_out_total($connect); ?></h1>
 				</div>
@@ -51,12 +55,17 @@ include('header.php');
 	-->
 	<div class="col-md-4">
 		<div class="panel panel-default">
-			<div class="panel-heading"><strong>Equipment Checked Out By You</strong></div>
+			<div class="panel-heading">
+				<strong>Equipment Checked Out By You</strong>
+			</div>
 			<div class="panel-body" align="center">
 				<h1><?php echo count_check_out_user($connect, $_SESSION['user_id']); ?></h1>
 			</div>
 		</div>
 	</div>
+
+</div> <!-- First Row -->
+<div class="row">
 
 	<!-- This block of code either outputs a table that shows currently checked out items, or a div that displays "All Equipment Accounted For" -->
 	<?php
@@ -80,26 +89,27 @@ include('header.php');
 	?>
 			<!--
 				Shows the currently checked out items, and the users who checked them out
-
-				THIS IS A STATIC TABLE. IT JUST DISPLAYS ITEMS CHECKED OUT ON THE CURRENT SYS DATE. 
 			-->
 			<div class="col-md-12">
 		        <div class="panel panel-default">
 		            <div class="panel-heading" style="font-size:1.4em;">
 		            	<center>
 			            	<strong>Currently Checked Out</strong>
-			           	<center>
 			            <button type="button" class="btn btn-link" data-toggle="popover" data-content="This table displays equipment still in use (or that hasn't been returned)." data-placement="left" style="position: absolute; top:-5px; right:10px">
 			            	<img src="images/info5_sm.png" alt="info">
 			           	</button>
+			           </center>
 		            </div>
 		                <?php echo table_checkouts($connect); ?>
 		        </div>
 		    </div>
-
-		<hr />
 	<?php
 		}
+	?>
+
+</div> <!-- Second Row -->
+<div class="row">
+	<?php
 	   	if(num_checkout_today($connect) <= 0){
 	?>
 	   		<div class="col-md-12">
@@ -143,40 +153,44 @@ include('header.php');
 	    }
 	}
 	?>
+</div> <!-- Third Row -->
+<hr />
+<div class="row">
+	<!--
+		Shows the currently checked out items for the currently logged in user
+	-->
+	<div class="col-md-12" style="">
+	    <div class="panel panel-default">
+	        <div class="panel-heading" style="font-size:1.4em;">
+	        	<center>
+	        		<strong>Your Equipment</strong>
+	        		<button type="button" class="btn btn-link" data-toggle="popover" data-content="This table displays all the equipment you have checked out recently." data-placement="left" style="position:absolute;top:-5px;right:10px">
+	                 	<img src="images/info5_sm.png" alt="info">
+	                </button>
+	        	</center>
+	        </div>
+	        <?php echo table_checkouts_user_wise($connect); ?>
+	        <div style="float:right">
+	        	<span class="glyphicon glyphicon-ok" style="text-size:1em;">
+	        		
+	        	</span> = Returned,
+	        	<span class="glyphicon glyphicon-remove" style="text-size:1em;">
+	        		
+	        	</span> = Still In Use
+	        </div>
+	    </div>
+	</div>
+</div>
 
-	<!-- This block of code displays the user's currently checked out items-->
-	<?php
-	if($_SESSION['type'] == 'user'){
-	?>
-		 <!--
-				Shows the currently checked out items, and the users who checked them out
 
-				THIS IS A STATIC TABLE. IT JUST DISPLAYS ITEMS CHECKED OUT ON THE CURRENT SYS DATE. 
-			-->
-			<div class="col-md-12">
-		        <div class="panel panel-default">
-		            <div class="panel-heading" style="font-size:1.4em;">
-		            	<center>
-		            		<strong>Your Equipment</strong>
-		            		<button type="button" class="btn btn-link" data-toggle="popover" data-content="This table displays all the equipment you have checked out recently." data-placement="left" style="position: absolute; top:-5px; right:10px">
-		                     	<img src="images/info5_sm.png" alt="info">
-		                    </button>
-		            	</center>
-		            </div>
-		                <?php echo table_checkouts_user_wise($connect); ?>
-		                <div style="float:right">
-		            		<span class="glyphicon glyphicon-ok" style="text-size:1em;"></span> = Returned,
-		            		<span class="glyphicon glyphicon-remove" style="text-size:1em;"></span> = Still In Use
-		            	</div>
-		        </div>
-		    </div>
-	<?php
-	}
-	?>
 
-    <?php
-	include("footer.php");
-	?>
+
+
+<!-- Importing the footer code -->    
+</div>
+<?php
+include("footer.php");
+?>
 
 
 
